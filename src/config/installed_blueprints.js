4512acfa2e137f2c04290a51452c78e0900773fe
Crowdbotics <team@crowdbotics.com>
2020-11-20 15:39:31 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,7 +2,6 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'SignUp21176431', human_name: 'Sign Up 2 1', access_route: 'SignUp21176431'},
 { name: 'SignIn22176430', human_name: 'Sign In 2 2', access_route: 'SignIn22176430'},
-{ name: 'Settings176414', human_name: 'Settings', access_route: 'Settings176414'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import SignUp21176431Navigator from '../features/SignUp21176431/navigator';
 import SignIn22176430Navigator from '../features/SignIn22176430/navigator';
-import Settings176414Navigator from '../features/Settings176414/navigator';
 
 /**
  * new navigators can be imported here
@@ -18,7 +17,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 SignUp21176431: { screen: SignUp21176431Navigator },
 SignIn22176430: { screen: SignIn22176430Navigator },
-Settings176414: { screen: Settings176414Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
